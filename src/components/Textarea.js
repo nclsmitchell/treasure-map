edee16ed4b8991ba8604bdafa4b87f241acd6393
nclsmitchell <ncls.mitchell@gmail.com>
2018-03-15 18:55:07 +0100

    fix init map

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -3,7 +3,7 @@ import '../css/textarea.css';
 
 class Textarea extends Component {
 	state = {
-		value: "C - 5 - 5\nM - 1 - 0\nM - 2 - 1\nM - 3 - 3\nM - 0- 2\nT - 0 - 3 - 2\nT - 1 - 3 - 3\nT - 4 - 0 - 2\nT - 4 - 2 - 1\nA - Lara - 1 - 1 - S - AADADAGGA\nA - Indiana - 3 - 0 - E - ADAAAADAAAADA\n# Add any row starting with M, T or A"
+		value: "C - 5 - 5\nM - 1 - 0\nM - 2 - 1\nM - 3 - 3\nM - 0 - 2\nT - 0 - 3 - 2\nT - 1 - 3 - 3\nT - 4 - 0 - 2\nT - 4 - 2 - 1\nA - Lara - 1 - 1 - S - AADADAGGA\nA - Indiana - 3 - 0 - E - ADAAAADAAAADA\n# Add any row starting with M, T or A"
 	};
 
 	handleChange = e => {
